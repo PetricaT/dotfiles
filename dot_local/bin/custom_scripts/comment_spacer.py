ad3a20c4823efb04dbe52f8eaa3117b9476cfb66
Petrica <24418935+PetricaT@users.noreply.github.com>
2026-04-07 19:36:56 +0300

    Changed to spaces, added comment utility script

--- dot_local/bin/custom_scripts/comment_spacer.py
+++ dot_local/bin/custom_scripts/comment_spacer.py
@@ -1,17 +1,31 @@
+#!/usr/bin/env python
+#
+# -- Simple util to normalize comments in .zshrc
+#   because I like it more that way :)
+#
 from pathlib import Path
+import os
 import regex
 
 # comment: str = "# ─"
-comment = regex.compile(r"#\s\─{1,40}\s[a-zA-z\s]{1,}\─{0,40}\n")
+comment = regex.compile(r"#\s[\─\-]{1,40}\s{1,}[a-zA-z\s]{1,}[\─\-]{0,40}\n")
 
-with open(Path.home() / ".zshrc.bak", "r+") as f:
-    for line in f:
-        if comment.match(line):
-            print(line)
+# rename the original file to .zshrc.bak
+if os.path.exists(Path.home() / ".zshrc.bak"):
+    os.remove(Path.home() / ".zshrc.bak")
+    os.rename(Path.home() / ".zshrc", Path.home() / ".zshrc.bak")
+else:
+    os.rename(Path.home() / ".zshrc", Path.home() / ".zshrc.bak")
 
-# for comment in comments:
-#     max_length = 80
-#     max_length -= len(comment) + 4 # Account for "# " and 2 spacers
+with open(Path.home() / ".zshrc.bak", "r") as source:
+    with open(Path.home() / ".zshrc", "w") as dest:
+        for line in source:
+            if comment.match(line):
+                line: str = line.replace("#", "").replace("-", "").replace("─", "").strip()
+                line: str = f" {line} "
+                dest.write(f"# {line:─^78}")
+            else:
+                dest.write(line)
 
-#     comment = f" {comment} "
-#     print(f"# {comment:─^78}")
+# Cleanup
+os.remove(Path.home() / ".zshrc.bak")
